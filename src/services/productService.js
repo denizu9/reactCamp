9aa56807852c8dba1dd382fa9c3dd1c34d560270
denizu9 <denizural9@gmail.com>
2023-08-04 16:58:25 +0300

    new commit

---
+++ src/services/productService.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export default class ProductService{
+    getProduct(){
+        return axios.get("http://localhost:8080/api/brands")
+    }
+
+    getModels(){
+        return axios.get("http://localhost:8080/api/models")
+    }
+}
